8bf726bf424d3a16c1643342a27839b881dedf67
hsengivc <hsengivc@proton.me>
2023-04-06 13:30:20 +0530

    Initial Commit

---
+++ FastingApp/FastingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastingAppApp.swift
+//  FastingApp
+//
+//  Created by Vignesh Chinnusamy on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct FastingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
